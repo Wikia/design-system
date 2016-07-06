7a2e01be7dd78729e63360d06d354da2c88e2a3f
Igor Rogatty <igor@wikia-inc.com>
2016-07-06 13:33:44 +0200

    DAT-4566 | split icons and logos into separate files

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,47 @@
-var gulp = require('gulp'),
+var fs = require('fs'),
+	gulp = require('gulp'),
 	path = require('path'),
 	rename = require('gulp-rename'),
 	svgmin = require('gulp-svgmin'),
 	svgstore = require('gulp-svgstore');
 
+function getFolders(dir) {
+	return fs.readdirSync(dir)
+		.filter(function (file) {
+			return fs.statSync(path.join(dir, file)).isDirectory();
+		});
+}
+
 gulp.task('svg', function () {
-	return gulp
-		.src('assets/**/*.svg', {
-			base: 'assets'
-		})
-		.pipe(rename(function (filePath) {
-			// Use `id="company-logo-wikia"` for company/logo-wikia.svg
-			var name = filePath.dirname.split(filePath.sep);
+	var svgRootDir = './assets',
+		folders = getFolders(svgRootDir);
 
-			name.push(filePath.basename);
-			filePath.basename = name.join('-');
-		}))
-		.pipe(svgmin(function (file) {
-			// Minify and make sure that we don't have duplicated ids in reusable elements
-			var prefix = path.basename(file.relative, path.extname(file.relative));
+	return folders.map(function (folder) {
+		return gulp
+			.src(path.join(svgRootDir, folder, '/*.svg'))
+			.pipe(rename(function (filePath) {
+				// Use `id="company-logo-wikia"` for company/logo-wikia.svg
+				filePath.basename = folder + '-' + filePath.basename;
+			}))
+			.pipe(svgmin(function (file) {
+				// Minify and make sure that we don't have duplicated ids in reusable elements
+				// Id of <symbol> element is set by svgstore based on the filename, not here
+				var prefix = folder + '-' + path.basename(file.relative, path.extname(file.relative));
 
-			return {
-				plugins: [{
-					cleanupIDs: {
-						prefix: prefix + '-',
-						minify: true
-					}
-				}]
-			}
-		}))
-		.pipe(svgstore({
-			inlineSvg: true
-		}))
-		.pipe(rename('symbols.svg'))
-		.pipe(gulp.dest('dist'));
+				return {
+					plugins: [{
+						cleanupIDs: {
+							prefix: prefix + '-',
+							minify: true
+						}
+					}]
+				}
+			}))
+			.pipe(svgstore({
+				inlineSvg: true
+			}))
+			.pipe(gulp.dest('dist'));
+	});
 });
 
 gulp.task('default', ['svg']);
