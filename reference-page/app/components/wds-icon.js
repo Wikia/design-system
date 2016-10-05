29c2a3d80808d7a7985dcdcc8406701d143e848d
Igor Rogatty <igor@wikia-inc.com>
2016-10-05 18:59:34 +0200

    XW-2165 | add all icons sizes

--- reference-page/app/components/wds-icon.js
+++ reference-page/app/components/wds-icon.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {Component, computed} = Ember;
+
+export default Component.extend({
+	iconClass: computed(function () {
+		const size = this.get('size');
+
+		return `wds-icon${size ? ` wds-icon-${size}` : ''}`;
+	}),
+	iconName: computed(function () {
+		const name = this.get('name'),
+			size = this.get('size');
+
+		return `wds-icons-${name}${size ? `-${size}` : ''}`;
+	})
 });
