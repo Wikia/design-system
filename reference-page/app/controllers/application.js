82f6985d5ae1e27d21f3e1b7a6ec05af7549481a
Igor Rogatty <igor@wikia-inc.com>
2016-10-05 12:57:15 +0200

    XW-2174 | add basic navigation

---
+++ reference-page/app/controllers/application.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+const {Controller} = Ember;
+
+export default Controller.extend({
+	navigation: [
+		{
+			name: 'Button',
+			location: 'component.button'
+		}
+	]
+});
