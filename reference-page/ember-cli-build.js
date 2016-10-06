cf6adbc00bba23a94a94838dacb4c9f63fcf95e5
Warkot <mateusz.warkocki@wikia-inc.com>
2016-10-06 17:09:18 +0200

    XW-2155 | add comment about highlight styles

--- reference-page/ember-cli-build.js
+++ reference-page/ember-cli-build.js
@@ -4,6 +4,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
 	var app = new EmberApp(defaults, {
+		// Styles can be previewed at https://highlightjs.org/static/demo/
+		// They are stored in ./node_modules/ember-cli-eg-code-highlight/vendor-addon/highlightjs/styles/
 		egCodeHighlight: {
 			style: 'github'
 		},
