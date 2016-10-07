3d4b16407bf37ec3ce93eabf38a826bdbe755fe7
Igor Rogatty <igor@wikia-inc.com>
2016-10-07 12:11:52 +0200

    XW-2165 | use icon-box component

--- reference-page/lib/wds-components/app/components/wds-icon.js
+++ reference-page/lib/wds-components/app/components/wds-icon.js
@@ -3,10 +3,18 @@ import Ember from 'ember';
 const {Component, computed} = Ember;
 
 export default Component.extend({
-	iconClass: computed(function () {
+	tagName: 'svg',
+	classNames: ['wds-icon'],
+	classNameBindings: ['sizeClassName'],
+	attributeBindings: ['width', 'height'],
+
+	width: 50,
+	height: 50,
+
+	sizeClassName: computed(function () {
 		const size = this.get('size');
 
-		return `wds-icon${size ? ` wds-icon-${size}` : ''}`;
+		return size ? `wds-icon-${size}` : '';
 	}),
 	iconName: computed(function () {
 		const name = this.get('name'),
