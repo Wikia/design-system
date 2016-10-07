b22e53bad20813d6919d04dd73d8c82a8494e60b
Igor Rogatty <igor@wikia-inc.com>
2016-10-07 16:11:34 +0200

    XW-2171 | show global footer in fullscreen

--- reference-page/app/components/component-demo.js
+++ reference-page/app/components/component-demo.js
@@ -1,12 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	classNames: ['component-demo'],
+
+	allowFullscreen: false,
 	codeOnly: false,
+	isInFullscreen: false,
 
 	didInsertElement() {
-		const $component = this.$('.rendered').clone();
+		const $component = this.$('.component-demo__rendered').clone();
 		$component.children().removeAttr('id').removeClass('ember-view');
 
 		this.set('code', $component.html());
+	},
+
+	actions: {
+		closeFullscreen() {
+			this.set('isInFullscreen', false);
+		},
+		showFullscreen() {
+			this.set('isInFullscreen', true);
+		}
 	}
 });
