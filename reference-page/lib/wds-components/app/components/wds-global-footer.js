df613c48a6658991974305e5a61304980edb4c91
Damian Jozwiak <damian@wikia-inc.com>
2016-10-07 16:57:22 +0200

    XW-2171 | Remove console.log

--- reference-page/lib/wds-components/app/components/wds-global-footer.js
+++ reference-page/lib/wds-components/app/components/wds-global-footer.js
@@ -3,8 +3,4 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName: 'footer',
 	classNames: 'wds-global-footer',
-
-	didInsertElement() {
-		console.log(this.get('model'));
-	}
 });
