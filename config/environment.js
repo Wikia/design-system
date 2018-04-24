66f408d686d81447f035cea2b56917cfd89e2c72
hakubo <bukaj.kelo+github@gmail.com>
2018-04-24 14:40:58 +0200

    update to Ember 3.1

--- config/environment.js
+++ config/environment.js
@@ -1,16 +1,35 @@
-/* jshint node: true */
+'use strict';
 
 module.exports = function (environment) {
-	var ENV = {
+	let ENV = {
 		modulePrefix: 'design-system',
-		environment: environment,
+		environment,
 		rootURL: '/',
 		locationType: 'hash',
 		EmberENV: {
-			FEATURES: {}
+			FEATURES: {
+				// Here you can enable experimental features on an ember canary build
+				// e.g. 'with-controller': true
+			},
+			EXTEND_PROTOTYPES: {
+				// Prevent Ember Data from overriding Date.parse.
+				Date: false
+			}
 		},
-		APP: {}
+
+		APP: {
+			// Here you can pass flags/options to your application instance
+			// when it is created
+		}
 	};
+
+	if (environment === 'development') {
+		// ENV.APP.LOG_RESOLVER = true;
+		// ENV.APP.LOG_ACTIVE_GENERATION = true;
+		// ENV.APP.LOG_TRANSITIONS = true;
+		// ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+		// ENV.APP.LOG_VIEW_LOOKUPS = true;
+	}
 
 	if (environment === 'test') {
 		// Testem prefers this...
@@ -21,6 +40,11 @@ module.exports = function (environment) {
 		ENV.APP.LOG_VIEW_LOOKUPS = false;
 
 		ENV.APP.rootElement = '#ember-testing';
+		ENV.APP.autoboot = false;
+	}
+
+	if (environment === 'production') {
+		// here you can enable a production-specific feature
 	}
 
 	if (environment === 'devbox') {
