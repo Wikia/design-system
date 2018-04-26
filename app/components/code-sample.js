f95890989e6517585c8a545626f5c42d97e340b3
hakubo <bukaj.kelo+github@gmail.com>
2018-04-26 23:36:15 +0200

    fix code samples

--- app/components/code-sample.js
+++ app/components/code-sample.js
@@ -1,18 +1,14 @@
 import Component from '@ember/component';
-import {run} from '@ember/runloop';
 import {registerHljsLanguage} from '../utils/beautify';
 
 export default Component.extend({
+	classNameBindings: ['language'],
 	language: 'html',
+	tagName: 'pre',
 
 	didInsertElement() {
-		const $code = this.$('code').get(0);
-
 		registerHljsLanguage();
 
-		// Wait for components/component-demo to beautify the HTML
-		run.schedule('afterRender', () => {
-			hljs.highlightBlock($code);
-		})
+		hljs.highlightBlock(this.element);
 	}
 });
