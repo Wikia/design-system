95c279b14e90f8c87131e270d879d59f2eba5209
hakubo <bukaj.kelo+github@gmail.com>
2018-04-30 14:49:42 +0200

    document component-demo

--- tests/dummy/app/components/component-demo.js
+++ tests/dummy/app/components/component-demo.js
@@ -2,15 +2,45 @@ import Component from '@ember/component';
 import ENV from '../config/environment';
 import beautify from '../utils/beautify';
 
+/**
+ * This components supports 3 different ways to render a component demo
+ * 
+ * Showcase template, rendered HTML and rendered component
+ * {{#component-demo name='unique-name'}}
+ * 	some code to demo
+ * {{/component-demo}}
+ * 
+ * Showcase template and rendered HTML but don't show rendered component
+ * {{#component-demo name='unique-name-2' codeOnly=true}}
+ *  other code to demo
+ * {{/component-demo}}
+ * under the hood it uses ember-code-snippet addont to get a snippet of code
+ * it uses clever way of searching through code for special markers
+ * that we have configured to be
+ * 	begin: /{{#component-demo[^}]+name='(\S+)'/,
+ * 	end: /{{\/component-demo}}/,
+ * This allows us to do easy component demos
+ * but it does not support multiline regexps, make sure whole component-demo invocation is on one line
+ * 
+ * Showcase rendered HTML only 
+ * {{#component-demo codeOnly=true}}
+ *  other code to demo
+ * {{/component-demo}}
+ */
 export default Component.extend({
 	classNames: ['component-demo'],
 
-	code: '',
+	// rendered code to demo
+	renderedComponent: '',
 	codeOnly: false,
 	rootURL: ENV.rootURL,
 	standalone: false,
 	standaloneDevice: null,
-
+	/**
+	 * name of a demo e.g. name='unique-name-of-a-demo'
+	 * This have to be globally unique name
+	*/
+	name: null,
 	showHTML: false,
 	showHBS: true,
 	language: 'handlebars',
@@ -25,7 +55,7 @@ export default Component.extend({
 
 			$component.find('.ember-view').removeAttr('id').removeClass('ember-view');
 	
-			this.set('code', beautify($component.html()));
+			this.set('renderedCode', beautify($component.html()));
 		}
 	},
 
