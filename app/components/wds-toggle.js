d7ebbb8c99fc05a26f4ac5a4f162610f916da1de
hakubo <bukaj.kelo+github@gmail.com>
2018-05-08 20:52:48 +0200

    auto id for wds-toggle

--- app/components/wds-toggle.js
+++ app/components/wds-toggle.js
@@ -1,10 +1,17 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
+import {guidFor} from '@ember/object/internals';
 
 export default Component.extend({
 	tagName: '',
 
 	checked: false,
 	disabled: false,
+
+	input: null,
+	id: computed(function() {
+		return guidFor(this);
+	}),
 
 	onChange() {},
 
