8d4da5424789a9331ed23af3e196c6b9759398ec
hakubo <bukaj.kelo+github@gmail.com>
2018-05-08 16:38:29 +0200

    small fixes

--- tests/dummy/app/helpers/i18n.js
+++ tests/dummy/app/helpers/i18n.js
@@ -2,10 +2,10 @@ import Helper from '@ember/component/helper';
 import {htmlSafe} from '@ember/string'
 import translations from 'npm:design-system-i18n/i18n/en/design-system';
 
-export function svg(params) {
+export function i18n(params) {
 	const key = params[0];
 
 	return htmlSafe(translations[key] || '');
 }
 
-export default Helper.helper(svg);
+export default Helper.helper(i18n);
