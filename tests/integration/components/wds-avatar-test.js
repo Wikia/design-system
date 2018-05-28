3dd16b3531db447b499a052534b113aa851d8048
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-05-28 12:27:38 +0200

    XW-4959: tests for wds-avatar component

---
+++ tests/integration/components/wds-avatar-test.js
@@ -0,0 +1,33 @@
+import {module, test} from 'qunit';
+import {setupRenderingTest} from 'ember-qunit';
+import {render} from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | wds-avatar', function (hooks) {
+	setupRenderingTest(hooks);
+
+	test('it renders default state', async function (assert) {
+		await render(hbs`{{wds-avatar}}`);
+
+		assert.equal(this.element.querySelectorAll('svg.wds-avatar__image').length, 1, 'should render default avatar svg');
+	});
+
+	test('it renders provided image', async function (assert) {
+		await render(hbs`{{wds-avatar src="/images/FANDOM-Avatar.jpg"}}`);
+
+		assert.equal(this.element.querySelectorAll('img[src="/images/FANDOM-Avatar.jpg"]').length, 1, 'should render provided avatar image');
+	});
+
+	test('it renders as link to provided url', async function (assert) {
+		await render(hbs`{{wds-avatar link="http://example.com"}}`);
+
+		assert.equal(this.element.querySelectorAll('a[href="http://example.com"]').length, 1, 'should render link');
+	});
+
+	test('it renders badge if provided', async function (assert) {
+		this.set('t', function(key) {return key});
+		await render(hbs`{{wds-avatar badge="admin" translateFunc=t}}`);
+
+		assert.equal(this.element.querySelectorAll('.wds-avatar__badge').length, 1, 'should render badge');
+	});
+});
