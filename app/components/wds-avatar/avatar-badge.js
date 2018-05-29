d2034fe0953a0f1b02bd0f83764c36fe0637f4a7
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-05-29 15:16:34 +0200

    XW-4998: i18n service for reference page

--- app/components/wds-avatar/avatar-badge.js
+++ app/components/wds-avatar/avatar-badge.js
@@ -1,7 +1,10 @@
 import Component from '@ember/component';
 import {computed} from '@ember/object';
+import {inject as service} from '@ember/service';
 
 export default Component.extend({
+	i18n: service(),
+
 	tagName: 'span',
 	classNames: 'wds-avatar__badge',
 	attributeBindings: ['title'],
@@ -17,6 +20,6 @@ export default Component.extend({
 	title: computed('name', function() {
 		const name = this.get('name');
 
-		return name ? this.get('translateFunc')(`wds-avatar-badges-${name}-tooltip`) : '';
+		return name ? this.get('i18n').t(`design-system:wds-avatar-badges-${name}-tooltip`) : '';
 	}),
 });
