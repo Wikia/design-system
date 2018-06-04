90ae7142fa8d3e209a5adcade0541d7cbd8ab633
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-06-04 10:46:51 +0200

    XW-4981 | Make eslint ignore one console statement

--- tests/dummy/app/services/logger.js
+++ tests/dummy/app/services/logger.js
@@ -2,6 +2,8 @@ import Service from '@ember/service';
 
 export default Service.extend({
 	error(name, data) {
+		/* eslint no-console:0 */
 		console.error(name, data);
+		/* eslint no-console:2 */
 	}
 });
