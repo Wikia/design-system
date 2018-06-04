ae15119a7e35f2d363fc411a791a56449f0f7c37
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-06-04 11:01:59 +0200

    XW-4981 | Eslint

--- tests/dummy/app/services/logger.js
+++ tests/dummy/app/services/logger.js
@@ -2,8 +2,8 @@ import Service from '@ember/service';
 
 export default Service.extend({
 	error(name, data) {
-		/* eslint no-console:0 */
+		/* eslint-disable no-console */
 		console.error(name, data);
-		/* eslint no-console:2 */
+		/* eslint-enable no-console */
 	}
 });
