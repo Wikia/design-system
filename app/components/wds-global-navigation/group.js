59efb893aed2eaad108adc375b7b0ecf3a245b9a
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-06-05 10:35:56 +0200

    XW-4978: fixing letter case in wikis dropdown, some cleanup

--- app/components/wds-global-navigation/group.js
+++ app/components/wds-global-navigation/group.js
@@ -1,7 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-	// tagName: 'a',
-	// attributeBindings: ['model.href:href'],
-	classNames: ['wds-global-navigation__link']
+	tagName: '',
 });
