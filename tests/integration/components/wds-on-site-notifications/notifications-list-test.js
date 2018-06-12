38b2f33de1db166ff88798a8269ab17820242511
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-06-12 14:18:37 +0200

    XW-5021 | Refactoring notification-list tests to use nested modules

--- tests/integration/components/wds-on-site-notifications/notifications-list-test.js
+++ tests/integration/components/wds-on-site-notifications/notifications-list-test.js
@@ -31,7 +31,7 @@ function getModelStub(length) {
 	return new EmberObject({ data });
 }
 
-module('Integration | Component | wds-on-site-notifications/notifications-list with loaded results', function (hooks) {
+module('Integration | Component | wds-on-site-notifications/notifications-list', function (hooks) {
 	setupRenderingTest(hooks);
 
 	hooks.beforeEach(function () {
@@ -40,70 +40,65 @@ module('Integration | Component | wds-on-site-notifications/notifications-list w
 				return 'some string';
 			}
 		}));
-
-		this.owner.register('service:notifications', Service.extend({
-			isLoading: false,
-
-			init() {
-				this._super(...arguments);
-				this.set('model', getModelStub(2));
-			}
-		}));
 	});
 
-	test('it should render as a UL element with proper classnames', async function (assert) {
-		await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
+	module('with loaded results', function (hooks) {
+		hooks.beforeEach(function () {
+			this.owner.register('service:notifications', Service.extend({
+				isLoading: false,
 
-		assert.equal(this.element.querySelector('ul').classList.contains('wds-notifications__notification-list'), true);
-		assert.equal(this.element.querySelector('ul').classList.contains('wds-list'), true);
-		assert.equal(this.element.querySelector('ul').classList.contains('wds-has-lines-between'), true);
+				init() {
+					this._super(...arguments);
+					this.set('model', getModelStub(2));
+				}
+			}));
+		});
+
+		test('it should render as a UL element with proper classnames', async function (assert) {
+			await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
+
+			assert.equal(this.element.querySelector('ul').classList.contains('wds-notifications__notification-list'), true);
+			assert.equal(this.element.querySelector('ul').classList.contains('wds-list'), true);
+			assert.equal(this.element.querySelector('ul').classList.contains('wds-has-lines-between'), true);
+		});
+
+		test('it should render a proper number of notification-card components', async function (assert) {
+			await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
+
+			assert.equal(this.element.querySelectorAll('.wds-notification-card').length, 2);
+		});
+
+		test('it should not render loading spinner', async function (assert) {
+			await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
+
+			assert.equal(this.element.querySelectorAll('.wds-spinner').length, 0);
+		});
 	});
 
-	test('it should render a proper number of notification-card components', async function (assert) {
-		await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
+	module('when loading results', function (hooks) {
+		hooks.beforeEach(function () {
+			this.owner.register('service:notifications', Service.extend({
+				isLoading: true,
 
-		assert.equal(this.element.querySelectorAll('.wds-notification-card').length, 2,
-			'should render two wds-notification-card components');
-	});
+				init() {
+					this._super(...arguments);
+					this.set('model', getModelStub(0));
+				}
+			}));
+		});
 
-	test('it should not render loading spinner', async function (assert) {
-		await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
+		test('it should render as a UL element with proper classnames', async function (assert) {
+			await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
 
-		assert.equal(this.element.querySelectorAll('.wds-spinner').length, 0);
-	});
-});
+			assert.equal(this.element.querySelector('ul').classList.contains('wds-notifications__notification-list'), true);
+			assert.equal(this.element.querySelector('ul').classList.contains('wds-list'), true);
+			assert.equal(this.element.querySelector('ul').classList.contains('wds-has-lines-between'), true);
+		});
 
-module('Integration | Component | wds-on-site-notifications/notifications-list when loading results', function (hooks) {
-	setupRenderingTest(hooks);
+		test('it should render loading spinner', async function (assert) {
+			await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
 
-	hooks.beforeEach(function () {
-		this.owner.register('service:i18n', Service.extend({
-			t() {
-				return 'some string';
-			}
-		}));
-
-		this.owner.register('service:notifications', Service.extend({
-			isLoading: true,
-
-			init() {
-				this._super(...arguments);
-				this.set('model', getModelStub(0));
-			}
-		}));
-	});
-
-	test('it renders as a UL element with proper classnames', async function (assert) {
-		await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
-
-		assert.equal(this.element.querySelector('ul').classList.contains('wds-notifications__notification-list'), true);
-		assert.equal(this.element.querySelector('ul').classList.contains('wds-list'), true);
-		assert.equal(this.element.querySelector('ul').classList.contains('wds-has-lines-between'), true);
-	});
-
-	test('it renders loading spinner', async function (assert) {
-		await render(hbs`{{wds-on-site-notifications/notifications-list}}`);
-
-		assert.equal(this.element.querySelectorAll('.wds-spinner').length, 1);
+			assert.equal(this.element.querySelectorAll('.wds-spinner').length, 1);
+		});
 	});
 });
