414ef4ece40ea8b1e4ad7a576717ec0ca2c39094
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-06-20 15:18:46 +0200

    XW-5041 | Creating search modal component

---
+++ app/components/wds-global-navigation/search-modal.js
@@ -0,0 +1,15 @@
+import {notEmpty, empty} from '@ember/object/computed';
+import Component from '@ember/component';
+import EmberObject from '@ember/object';
+import fetch from 'fetch';
+import {run} from '@ember/runloop';
+import wrapMeHelper from '../../helpers/wrap-me';
+import {inject as service} from '@ember/service';
+
+export default Component.extend({
+	actions: {
+		openModal(modalType) {
+			this.get('openModal')(modalType);
+		}
+	}
+});
