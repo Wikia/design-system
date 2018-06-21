3452e9228df6f5bc032d95cea631472dbb7c8b0a
Karol Tatała <ktatala@wikia-inc.com>
2018-06-21 12:22:22 +0200

    IRIS-6233 | community header as shareable component

--- app/components/wds-community-header/local-navigation-dropdown-level-2.js
+++ app/components/wds-community-header/local-navigation-dropdown-level-2.js
@@ -3,7 +3,7 @@ import {computed} from '@ember/object';
 
 export default Component.extend({
 	tagName: '',
-	stickedClassName: computed('model', 'currentIndex', function () {
-		return (this.get('model').submenu.length < this.get('currentIndex') + 1) ? 'wds-is-sticked-to-parent' : '';
+	stickedClassName: computed('item', 'currentIndex', function () {
+		return (this.get('item').items.length < this.get('currentIndex') + 1) ? 'wds-is-sticked-to-parent' : '';
 	})
 });
