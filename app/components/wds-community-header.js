eab6e1a38a5bb3635e3e216943792a113163ce03
Karol Tatała <ktatala@wikia-inc.com>
2018-06-21 12:46:03 +0200

    Fix background image, move partials

--- app/components/wds-community-header.js
+++ app/components/wds-community-header.js
@@ -1,11 +1,19 @@
 import Component from '@ember/component';
 import {computed} from '@ember/object';
+import {htmlSafe} from '@ember/string';
 
 export default Component.extend({
 	attributeBindings: ['style'],
 	classNames: 'wds-community-header',
 	tagName: 'header',
-	style: computed('model', function () {
-		return `background-image: url(${this.get('model')['background-image-url']});`
+
+	style: computed('model.background_image', function () {
+		const backgroundImage = this.get('model.background_image');
+
+		if (backgroundImage) {
+			return htmlSafe(`background-image: url('${backgroundImage}');`);
+		} else {
+			return htmlSafe('');
+		}
 	}),
 });
