0b667d5ad9ca1b8713315dda47f71c2c67cafe07
Karol Tatała <ktatala@wikia-inc.com>
2018-06-21 13:05:30 +0200

    IRIS-6233 | community header track function

--- app/components/wds-community-header.js
+++ app/components/wds-community-header.js
@@ -7,6 +7,14 @@ export default Component.extend({
 	classNames: 'wds-community-header',
 	tagName: 'header',
 
+	init() {
+		this._super(...arguments);
+
+		if(!this.track) {
+			throw new Error('Required `track` function for wds-community-header component is not set');
+		}
+	},
+
 	style: computed('model.background_image', function () {
 		const backgroundImage = this.get('model.background_image');
 
@@ -16,4 +24,12 @@ export default Component.extend({
 			return htmlSafe('');
 		}
 	}),
+
+	click(event) {
+		const elementToTrack = event.target.closest('[data-tracking-label]');
+
+		if (elementToTrack) {
+			this.track(elementToTrack.getAttribute('data-tracking-label'));
+		}
+	},
 });
