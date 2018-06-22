710f2e1a1667f66ff50b975098350ca75661023a
Karol Tatała <ktatala@wikia-inc.com>
2018-06-22 15:27:23 +0200

    IRIS-6238 | Global footer as shareable component

--- app/components/wds-global-footer.js
+++ app/components/wds-global-footer.js
@@ -3,4 +3,26 @@ import Component from '@ember/component';
 export default Component.extend({
 	tagName: 'footer',
 	classNames: 'wds-global-footer',
+
+	init() {
+		this._super(...arguments);
+
+		if(!this.track) {
+			// throw new Error('Required `track` function for wds-global-footer component is not set');
+		}
+	},
+
+	click(event) {
+		const elementToTrack = event.target.closest('[data-tracking-label]');
+
+		if (elementToTrack) {
+			this.track(elementToTrack.getAttribute('data-tracking-label'));
+		}
+	},
+
+	actions: {
+		track() {
+
+		}
+	}
 });
