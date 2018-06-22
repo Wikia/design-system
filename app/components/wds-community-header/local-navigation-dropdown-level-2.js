197d4ad0b2f7101cfd2ae7db142b30ca16f9b92a
Karol Tatała <ktatala@wikia-inc.com>
2018-06-22 12:55:23 +0200

    Rename back navigation items

--- app/components/wds-community-header/local-navigation-dropdown-level-2.js
+++ app/components/wds-community-header/local-navigation-dropdown-level-2.js
@@ -8,7 +8,7 @@ export default Component.extend({
 		return (this.get('item.items.length') < this.get('currentIndex') + 1) ? 'wds-is-sticked-to-parent' : '';
 	}),
 
-	itemHref: computed('item.href', function () {
-		return this.item.href || '#';
+	href: computed('model.href', function () {
+		return this.model.href || '#';
 	})
 });
