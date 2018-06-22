010f4d04163d60d9c4788b405e12e0729777bed6
Karol Tatała <ktatala@wikia-inc.com>
2018-06-22 11:47:25 +0200

    Pass item href to second level link

--- app/components/wds-community-header/local-navigation-dropdown-level-2.js
+++ app/components/wds-community-header/local-navigation-dropdown-level-2.js
@@ -3,7 +3,12 @@ import {computed} from '@ember/object';
 
 export default Component.extend({
 	tagName: '',
+
 	stickedClassName: computed('item', 'currentIndex', function () {
-		return (this.get('item').items.length < this.get('currentIndex') + 1) ? 'wds-is-sticked-to-parent' : '';
+		return (this.get('item.items.length') < this.get('currentIndex') + 1) ? 'wds-is-sticked-to-parent' : '';
+	}),
+
+	itemHref: computed('item.href', function () {
+		return this.item.href || '#';
 	})
 });
