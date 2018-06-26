1d3b056a0f3fa8a47861343fc4554ed3351d0944
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-06-26 09:30:50 +0200

    XW-5041 | Stubbing global-navigation controller and adding methods to it

---
+++ tests/dummy/app/controllers/identity/global-navigation.js
@@ -0,0 +1,13 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+	track(label) {
+		// eslint-disable-next-line no-console
+		console.info('tracking', {label});
+	},
+
+	onSearchSuggestionChosen({title}) {
+		// eslint-disable-next-line no-console
+		console.info('Suggestion clicked', title);
+	}
+});
