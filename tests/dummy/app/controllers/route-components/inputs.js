cbb8b00e735a5f5232e9915e5ff756257b3f9c0c
Karol Tatała <ktatala@wikia-inc.com>
2018-06-27 11:31:53 +0200

    Events support

--- tests/dummy/app/controllers/route-components/inputs.js
+++ tests/dummy/app/controllers/route-components/inputs.js
@@ -1,5 +1,22 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-	inputValue: 'initial value'
+	initialValue: 'initial value',
+
+	actions: {
+		input() {
+			// eslint-disable-next-line no-console
+			console.log('input');
+		},
+
+		focusOut() {
+			// eslint-disable-next-line no-console
+			console.log('focusOut');
+		},
+
+		focusIn() {
+			// eslint-disable-next-line no-console
+			console.log('focusIn');
+		}
+	}
 });
