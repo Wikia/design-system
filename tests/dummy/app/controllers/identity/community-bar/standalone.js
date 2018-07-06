ea020231b93b5813fba4d7fe6cd72ed37938635c
Karol Tatała <ktatala@wikia-inc.com>
2018-07-06 10:34:03 +0200

    Global Navigation + Community Bar Animation

--- tests/dummy/app/controllers/identity/community-bar/standalone.js
+++ tests/dummy/app/controllers/identity/community-bar/standalone.js
@@ -9,5 +9,15 @@ export default Controller.extend({
 	linkClicked(href) {
 		// eslint-disable-next-line no-console
 		console.info('linkClicked', { href })
+	},
+
+	onSearchSuggestionChosen({ title }) {
+		// eslint-disable-next-line no-console
+		console.info('Suggestion clicked', title);
+	},
+
+	goToSearchResults(query) {
+		// eslint-disable-next-line no-console
+		console.info('goToSearchResults triggered', query);
 	}
 });
