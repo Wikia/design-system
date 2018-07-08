a3441f74299c877f28280ada2cb7f978362d040b
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-07-08 15:32:54 +0200

    Revert "XW-4947: redirect param for signin/register links"
    This reverts commit 74b3825d3e3e20d20208ace4e1561be25773d6a7.

--- app/components/wds-global-navigation/anon.js
+++ app/components/wds-global-navigation/anon.js
@@ -1,20 +1,5 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
 	tagName: '',
-	signinUrl: computed('model', function() {
-		const signin = this.get('model.signin');
-
-		return this.getFullUrl(signin);
-	}),
-	registerUrl: computed('model', function() {
-		const register = this.get('model.register');
-
-		return this.getFullUrl(register);
-	}),
-
-	getFullUrl(link) {
-		return `${link.href}?${link['param-name']}=${encodeURIComponent(window.location.href)}`;
-	}
 });
