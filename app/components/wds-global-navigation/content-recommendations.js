8158b1ee22c9f559c8ca631af7c831741a241504
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-07-16 15:06:40 +0200

    XW-5046 | Infinite scroll, code style

--- app/components/wds-global-navigation/content-recommendations.js
+++ app/components/wds-global-navigation/content-recommendations.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
+import {computed} from '@ember/object';
+import {inject as service} from '@ember/service';
 
 const recircItemsCount = 50,
 	config = {
@@ -11,70 +12,93 @@ const recircItemsCount = 50,
 			resultType: 'cross-domain',
 			domainType: 'fandom.wikia.com'
 		}
-};
+	};
 
 export default Component.extend({
 	wdsLiftigniter: service(),
+	classNames: ['wds-content-recommendations'],
 	tagName: 'div',
+	displayedItemsCount: 10,
+	displayedItems: computed('model', 'displayedItemsCount', function () {
+		return this.model ? this.model.slice(0, this.get('displayedItemsCount')) : [];
+	}),
+
 	init() {
 		this._super(...arguments);
 		this.wdsLiftigniter.initLiftigniter({});
 		this.fetchLiftIgniterData();
+		this.onScroll = this.onScroll.bind(this);
 	},
-    fetchLiftIgniterData() {
-        const wdsLiftigniter = this.wdsLiftigniter;
 
-	    wdsLiftigniter
-            .getData(config)
-            .then((data) => {
-            	console.log(data)
-	            this.set('model', data.items.filter((item) => {
-	            	return item.hasOwnProperty('thumbnail') && item.thumbnail;
-		        })
-		            .slice(0, recircItemsCount))
-                //     .map((item) => {
-                //         item.thumbnail = Thumbnailer.getThumbURL(item.thumbnail, {
-                //             mode: Thumbnailer.mode.scaleToWidth,
-                //             width: normalizeThumbWidth(window.innerWidth)
-                //         });
-                //
-                //         return item;
-                });
+	didInsertElement() {
+		this.searchModal = document.querySelector('.wds-search-modal');
 
-                // run.scheduleOnce('afterRender', () => {
-                //     if (!this.isDestroyed) {
-                //         liftigniter.setupTracking(
-                //             this.element.querySelectorAll('.recirculation-prefooter__item'),
-                //             config.widget,
-                //             'LI'
-                //         );
-                //         this.get('listRendered').resolve();
-                //     }
-                // });
+		this.searchModal.addEventListener('scroll', this.onScroll);
+	},
 
-                // if (this.shouldShowPlista) {
-                //     this.fetchPlista()
-                //         .then(this.mapPlista)
-                //         .then((item) => {
-                //             if (item.thumbnail) {
-                //                 let newItems = this.items;
-                //
-                //                 newItems.splice(1, 0, item);
-                //                 newItems.pop();
-                //                 this.set('items', newItems);
-                //                 this.notifyPropertyChange('items');
-                //             }
-                //         })
-                //         .catch((error) => {
-                //             this.logger.info('Plista fetch failed', error);
-                //         });
-                // }
-            // });
+	willDestroyElement() {
+		this.searchModal.removeEventListener('scroll', this.onScroll);
+	},
 
-        // track({
-        //           action: trackActions.impression,
-        //           category: 'recirculation',
-        //           label: 'footer'
-        //       });
-    },
+	onScroll() {
+		if (this.searchModal.scrollTop === (this.searchModal.scrollHeight - this.searchModal.offsetHeight)) {
+			this.set('displayedItemsCount', this.get('displayedItemsCount') + 10);
+		}
+	},
+
+	fetchLiftIgniterData() {
+		const wdsLiftigniter = this.wdsLiftigniter;
+
+		wdsLiftigniter
+			.getData(config)
+			.then((data) => {
+				this.set('model', data.items.filter((item) => {
+					return item.hasOwnProperty('thumbnail') && item.thumbnail;
+				})
+					.slice(0, recircItemsCount))
+				//     .map((item) => {
+				//         item.thumbnail = Thumbnailer.getThumbURL(item.thumbnail, {
+				//             mode: Thumbnailer.mode.scaleToWidth,
+				//             width: normalizeThumbWidth(window.innerWidth)
+				//         });
+				//
+				//         return item;
+			});
+
+		// run.scheduleOnce('afterRender', () => {
+		//     if (!this.isDestroyed) {
+		//         liftigniter.setupTracking(
+		//             this.element.querySelectorAll('.recirculation-prefooter__item'),
+		//             config.widget,
+		//             'LI'
+		//         );
+		//         this.get('listRendered').resolve();
+		//     }
+		// });
+
+		// if (this.shouldShowPlista) {
+		//     this.fetchPlista()
+		//         .then(this.mapPlista)
+		//         .then((item) => {
+		//             if (item.thumbnail) {
+		//                 let newItems = this.items;
+		//
+		//                 newItems.splice(1, 0, item);
+		//                 newItems.pop();
+		//                 this.set('items', newItems);
+		//                 this.notifyPropertyChange('items');
+		//             }
+		//         })
+		//         .catch((error) => {
+		//             this.logger.info('Plista fetch failed', error);
+		//         });
+		// }
+		// });
+
+		// track({
+		//           action: trackActions.impression,
+		//           category: 'recirculation',
+		//           label: 'footer'
+		//       });
+	},
 });
--- app/services/wds-liftigniter.js
+++ app/services/wds-liftigniter.js
@@ -1,94 +1,94 @@
 import Service from '@ember/service';
-import { Promise } from 'rsvp';
+import {Promise} from 'rsvp';
 // import localStorageConnector from '../utils/local-storage-connector';
 
 export default Service.extend({
 	initLiftigniter(adsContext) {
-	  const kxallsegs = localStorage.getItem('kxallsegs');
-	  let context = {};
+		const kxallsegs = localStorage.getItem('kxallsegs');
+		let context = {};
 
-	  if (adsContext && adsContext.targeting) {
-	      const targeting = adsContext.targeting;
+		if (adsContext && adsContext.targeting) {
+			const targeting = adsContext.targeting;
 
-	      context = {
-	          _wCategory: targeting.wikiCategory,
-	          _wName: targeting.wikiDbName,
-	          _wTop: targeting.wikiIsTop1000,
-	          _wikiLanguage: targeting.wikiLanguage,
-	          _wVert: targeting.wikiVertical,
-	          _pType: targeting.pageType,
-	          _pName: targeting.pageName
-	      };
+			context = {
+				_wCategory: targeting.wikiCategory,
+				_wName: targeting.wikiDbName,
+				_wTop: targeting.wikiIsTop1000,
+				_wikiLanguage: targeting.wikiLanguage,
+				_wVert: targeting.wikiVertical,
+				_pType: targeting.pageType,
+				_pName: targeting.pageName
+			};
 
-	      if (targeting.wikiCustomKeyValues) {
-	          targeting.wikiCustomKeyValues
-	              .split(';')
-	              .map((keyVal) => {
-	                  return keyVal.split('=');
-	              })
-	              .forEach((parts) => {
-	                  const key = `_${parts[0]}`;
+			if (targeting.wikiCustomKeyValues) {
+				targeting.wikiCustomKeyValues
+					.split(';')
+					.map((keyVal) => {
+						return keyVal.split('=');
+					})
+					.forEach((parts) => {
+						const key = `_${parts[0]}`;
 
-	                  if (!context[key]) {
-	                      context[key] = [parts[1]];
-	                  } else {
-	                      context[key].push(parts[1]);
-	                  }
-	              });
-	      }
-	  }
+						if (!context[key]) {
+							context[key] = [parts[1]];
+						} else {
+							context[key].push(parts[1]);
+						}
+					});
+			}
+		}
 
-	  if (kxallsegs) {
-	      context._kruxTags = kxallsegs.split(',');
-	  }
+		if (kxallsegs) {
+			context._kruxTags = kxallsegs.split(',');
+		}
 
-	  window.liftigniter('init', 'l9ehhrb6mtv75bp2', {
-	      config: {
-	          globalCtx: context,
-	      }
-	  });
+		window.liftigniter('init', 'l9ehhrb6mtv75bp2', {
+			config: {
+				globalCtx: context,
+			}
+		});
 
-	  window.liftigniter('send', 'pageview');
-	  window.liftigniter('setRequestFields',
-	                     ['rank', 'thumbnail', 'title', 'url',
-	                      'presented_by', 'author']);
+		window.liftigniter('send', 'pageview');
+		window.liftigniter('setRequestFields',
+			['rank', 'thumbnail', 'title', 'url',
+				'presented_by', 'author']);
 	},
 
 	getData(config) {
-	  return new Promise((resolve, reject) => {
-	      const registerOptions = {
-	          max: config.max,
-	          widget: config.widget,
-	          callback: resolve
-	      };
+		return new Promise((resolve, reject) => {
+			const registerOptions = {
+				max: config.max,
+				widget: config.widget,
+				callback: resolve
+			};
 
-	      if (!window.liftigniter) {
-	          return reject(
-	              new Error('Liftigniter library not found'));
-	      }
+			if (!window.liftigniter) {
+				return reject(
+					new Error('Liftigniter library not found'));
+			}
 
-	      if (config.opts) {
-	          registerOptions.opts = config.opts;
-	      }
+			if (config.opts) {
+				registerOptions.opts = config.opts;
+			}
 
-	      // currently we display only one recirc component on a
-	      // page so calling 'fetch' with every invocation of this
-	      // method is fine. However, if there will be more than
-	      // one recirc component on a page, 'register' should be
-	      // called for every of them, and the fetch only once at
-	      // the end - the calls to liftigniter will be batched.
-	      window.liftigniter('register', registerOptions);
-	      window.liftigniter('fetch');
-	  });
+			// currently we display only one recirc component on a
+			// page so calling 'fetch' with every invocation of this
+			// method is fine. However, if there will be more than
+			// one recirc component on a page, 'register' should be
+			// called for every of them, and the fetch only once at
+			// the end - the calls to liftigniter will be batched.
+			window.liftigniter('register', registerOptions);
+			window.liftigniter('fetch');
+		});
 	},
 
 	setupTracking(elements, widgetName, source) {
-	  const options = {
-	      elements,
-	      name: widgetName,
-	      source
-	  };
+		const options = {
+			elements,
+			name: widgetName,
+			source
+		};
 
-	  window.liftigniter('track', options);
+		window.liftigniter('track', options);
 	}
 });
