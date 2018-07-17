55f11734b133d7a90851b95843b6340cd0615467
rogatty <igor@wikia-inc.com>
2018-07-17 10:08:00 +0200

    IRIS-6402 | allow avatar stack count to be overriden

--- app/components/wds-avatar-stack.js
+++ app/components/wds-avatar-stack.js
@@ -5,16 +5,22 @@ export default Component.extend({
 	classNames: ['wds-avatar-stack'],
 	maxStackSize: 5,
 	avatars: null,
+	overrideCount: null,
 
-	overflow: computed('avatars', function() {
-		const count = this.get('avatars').length || 0;
+	count: computed('avatars', 'overrideCount', function () {
+		return this.overrideCount || this.get('avatars.length') || 0;
+	}),
+
+	overflow: computed('avatars', function () {
+		const count = this.get('count');
 		const maxStackSize = this.get('maxStackSize');
 
 		return count > maxStackSize ? count - maxStackSize : 0;
 	}),
-	displayableAvatars: computed('avatars', 'maxStackSize', function() {
+
+	displayableAvatars: computed('avatars', 'maxStackSize', function () {
 		const avatars = this.get('avatars') || [];
-		const count = avatars.length;
+		const count = this.get('count');
 		const maxStackSize = this.get('maxStackSize');
 
 		return count > maxStackSize ? avatars.slice(0, maxStackSize) : avatars;
