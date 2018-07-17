141da3e1544209a8aecc9613e2f391c37f8776fe
rogatty <igor@wikia-inc.com>
2018-07-17 15:41:14 +0200

    IRIS-6402 | allow hiding the overflow text

--- app/components/wds-avatar-stack.js
+++ app/components/wds-avatar-stack.js
@@ -5,17 +5,20 @@ export default Component.extend({
 	classNames: ['wds-avatar-stack'],
 	maxStackSize: 5,
 	avatars: null,
+	hideOverflow: false,
 	overrideCount: null,
 
 	count: computed('avatars', 'overrideCount', function () {
 		return this.overrideCount || this.get('avatars.length') || 0;
 	}),
 
-	overflow: computed('count', 'maxStackSize', function () {
-		return this.count > this.maxStackSize ? this.count - this.maxStackSize : 0;
+	overflow: computed('count', 'hideOverflow', 'maxStackSize', function () {
+		return !this.hideOverflow && this.count > this.maxStackSize ?
+			this.count - this.maxStackSize :
+			0;
 	}),
 
-	displayableAvatars: computed('avatars', 'maxStackSize', function () {
+	displayableAvatars: computed('avatars', 'count', 'maxStackSize', function () {
 		const avatars = this.avatars || [];
 
 		return this.count > this.maxStackSize ? avatars.slice(0, this.maxStackSize) : avatars;
