fe73424d6075bfe1cff96fc3fa61b11212ef00ea
bkoval <brt.kowal@gmail.com>
2018-07-19 13:35:48 +0200

    XW-5046 | Adding tracking to trending in search

--- app/components/wds-global-navigation/content-recommendation-card.js
+++ app/components/wds-global-navigation/content-recommendation-card.js
@@ -6,4 +6,20 @@ export default Component.extend({
 	classNames: ['wds-content-recommendations__card'],
 	attributeBindings: ['href'],
 	href: readOnly('model.url'),
+
+	click(event) {
+		let label;
+
+		if (event.target.classList.contains('wds-content-recommendations__card-thumbnail')) {
+			label = 'search-trending-thumbnail';
+		} else {
+			label = 'search-trending-card';
+		}
+
+		this.track && this.track({
+			label,
+			action: 'click',
+			category: 'recirculation'
+		});
+	}
 });
