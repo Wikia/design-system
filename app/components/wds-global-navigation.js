f70df94fcdd5e907e2dec148ad0cdb85a69283ab
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-08-29 10:59:26 +0200

    XW-5110: calculate check relative position of communityBar to globalNav in order to switch to glocal nav

--- app/components/wds-global-navigation.js
+++ app/components/wds-global-navigation.js
@@ -26,7 +26,6 @@ export default Component.extend({
 	searchIsActive: false,
 	communityBarIsActive: false,
 	previousViewportOffsetTop: null,
-	scrollPositionWhenStickedToTop: 0,
 
 	isSearchModalOpen: equal('currentModal', 'search'),
 	isUserModalOpen: equal('currentModal', 'user'),
@@ -72,24 +71,19 @@ export default Component.extend({
 	},
 
 	scrollHandler() {
-		const globalNavigationHeight = 55;
-		const viewportOffsetTop = this.element.getBoundingClientRect().top;
+		const communityBar = document.querySelector('.wds-community-bar');
+		const communityBarOffsetY = communityBar ? communityBar.getBoundingClientRect().top : 0;
+		const globalNavOffsetY = this.element.getBoundingClientRect().top;
 
-		if (this.previousViewportOffsetTop !== 0 && viewportOffsetTop === 0) {
-			this.scrollPositionWhenStickedToTop = window.pageYOffset;
-		}
-
-		if (window.pageYOffset > this.scrollPositionWhenStickedToTop + globalNavigationHeight && this.communityBarIsActive === false) {
+		if (communityBarOffsetY <= globalNavOffsetY && this.communityBarIsActive === false) {
 			run(() => {
 				this.set('communityBarIsActive', true);
 			});
-		} else if (window.pageYOffset <= this.scrollPositionWhenStickedToTop + globalNavigationHeight && this.communityBarIsActive === true) {
+		} else if (communityBarOffsetY > globalNavOffsetY && this.communityBarIsActive === true) {
 			run(() => {
 				this.set('communityBarIsActive', false);
 			});
 		}
-
-		this.previousViewportOffsetTop = viewportOffsetTop;
 	},
 
 	openModal(modalType) {
