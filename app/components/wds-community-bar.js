2cff77600c088d449eaaac01022808ba3d732a56
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-08-30 10:03:36 +0200

    XW-5099: pass event to local navigation link click handler

--- app/components/wds-community-bar.js
+++ app/components/wds-community-bar.js
@@ -40,7 +40,7 @@ export default Component.extend({
 		if (!href || href === '#') {
 			event.preventDefault();
 		} else {
-			this.onLinkClicked(href);
+			this.onLinkClicked(href, event);
 		}
 	}
 });
