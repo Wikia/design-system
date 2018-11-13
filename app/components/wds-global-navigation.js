6fc1378e4fdf0bc5984568a56b30b96e50dde302
rogatty <igor@wikia-inc.com>
2018-11-13 14:44:57 +0100

    PLATFORM-3800 | remove obsolete check

--- app/components/wds-global-navigation.js
+++ app/components/wds-global-navigation.js
@@ -77,7 +77,7 @@ export default Component.extend({
 	},
 
 	scrollHandler() {
-		const communityBarOffsetY = this.communityBarElement ? this.communityBarElement.getBoundingClientRect().top : 0;
+		const communityBarOffsetY = this.communityBarElement.getBoundingClientRect().top;
 		const globalNavOffsetY = this.element.getBoundingClientRect().top;
 
 		if (communityBarOffsetY <= globalNavOffsetY && this.communityBarIsActive === false) {
