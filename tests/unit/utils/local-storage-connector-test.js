b5af3d0383896a1d62930ba22d6bc3197299f388
hakubo <bukaj.kelo+github@gmail.com>
2018-12-12 09:41:48 +0100

    add test for local-storage-connector

--- tests/unit/utils/local-storage-connector-test.js
+++ tests/unit/utils/local-storage-connector-test.js
@@ -1,9 +1,27 @@
 import localStorageConnector from 'dummy/utils/local-storage-connector';
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 
 module('Unit | Utility | local-storage-connector', function() {
-	skip('it works', function(assert) {
-		let result = localStorageConnector();
-		assert.ok(result);
+	test('getItem/setItem works', assert => {
+		const localStorageAdapter = localStorageConnector.localStorageAdapter;
+
+		localStorageAdapter.setItem('foo', 'bar');
+		assert.strictEqual(localStorageAdapter.getItem('foo'), 'bar');
+	});
+
+	test('clear works', assert => {
+		const localStorageAdapter = localStorageConnector.localStorageAdapter;
+
+		localStorageAdapter.setItem('foo', 'bar');
+		localStorageAdapter.removeItem('foo');
+		assert.strictEqual(localStorageAdapter.getItem('foo') || false, false);
+	});
+
+	test('removeItem works', assert => {
+		const localStorageAdapter = localStorageConnector.localStorageAdapter;
+
+		localStorageAdapter.setItem('foo', 'bar');
+		localStorageAdapter.removeItem('foo');
+		assert.strictEqual(localStorageAdapter.getItem('foo') || false, false);
 	});
 });
