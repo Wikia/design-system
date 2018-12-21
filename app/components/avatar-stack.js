230e8a6b0ca79e33c31b6e38bc9701bde0b8ac8d
hakubo <bukaj.kelo+github@gmail.com>
2018-12-21 14:37:39 +0100

    fixes for avatar-stack

--- app/components/avatar-stack.js
+++ app/components/avatar-stack.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { assert } from '@ember/debug';
 
 export default Component.extend({
 	classNames: ['wds-avatar-stack'],
@@ -8,19 +9,30 @@ export default Component.extend({
 	hideOverflow: false,
 	overrideCount: null,
 
-	count: computed('avatars', 'overrideCount', function () {
-		return this.overrideCount || this.get('avatars.length') || 0;
+	init() {
+		this._super(...arguments);
+
+		assert('@avatars are required', this.avatars instanceof Array);
+	},
+
+	count: computed('avatars', 'overrideCount', function() {
+		return this.overrideCount || this.avatars.length || 0;
 	}),
 
-	overflow: computed('count', 'hideOverflow', 'maxStackSize', function () {
-		return !this.hideOverflow && this.count > this.maxStackSize ?
-			this.count - this.maxStackSize :
-			0;
+	overflow: computed('count', 'hideOverflow', 'maxStackSize', function() {
+		return !this.hideOverflow && this.count > this.maxStackSize
+			? this.count - this.maxStackSize
+			: 0;
 	}),
 
-	displayableAvatars: computed('avatars', 'count', 'maxStackSize', function () {
-		const avatars = this.avatars || [];
-
-		return this.count > this.maxStackSize ? avatars.slice(0, this.maxStackSize) : avatars;
-	}),
+	displayableAvatars: computed(
+		'avatars',
+		'count',
+		'maxStackSize',
+		function() {
+			return this.count > this.maxStackSize
+				? this.avatars.slice(0, this.maxStackSize)
+				: this.avatars;
+		},
+	),
 });
