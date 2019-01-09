1f686c79c4afc4b40037776659ecf4c4bb582ff2
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-01-09 09:09:24 +0100

    ADEN-8154 Expose GlobalNavigation.onModalOpen action

--- tests/integration/components/global-navigation-test.js
+++ tests/integration/components/global-navigation-test.js
@@ -1,35 +1,50 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import Service from '@ember/service';
 import model from 'dummy/models/global-navigation';
 
-module('Integration | Component | global-navigation', function(hooks) {
-  setupRenderingTest(hooks);
+module('Integration | Component | global-navigation', function (hooks) {
+	setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-	this.owner.register(
-		'service:i18n',
-		Service.extend({
-			t(key) {
-				return key;
-			},
-		}),
-	);
-
-	this.set('model', model['logged-out-default']);
-	await render(hbs`
-		<GlobalNavigation @model={{model}}/>
-	`);
-
-	assert.dom('.wds-global-navigation').exists();
-	assert.dom('.wds-global-navigation__logo').exists();
-	assert.dom('.wds-global-navigation__link').exists({ count: 5 });
-	assert
-		.dom('.wds-global-navigation__search')
-		.hasText(
-			'design-system:global-navigation-search-placeholder-inactive',
+	test('it renders', async function (assert) {
+		this.owner.register(
+			'service:i18n',
+			Service.extend({
+				t(key) {
+					return key;
+				},
+			}),
 		);
-  });
+
+		this.set('model', model['logged-out-default']);
+		await render(hbs`
+			<GlobalNavigation @model={{model}}/>
+		`);
+
+		assert.dom('.wds-global-navigation').exists();
+		assert.dom('.wds-global-navigation__logo').exists();
+		assert.dom('.wds-global-navigation__link').exists({count: 5});
+		assert
+			.dom('.wds-global-navigation__search')
+			.hasText(
+				'design-system:global-navigation-search-placeholder-inactive',
+			);
+	});
+
+	test('search input results are shown after click on submit button', async function (assert) {
+		this.set('onModalOpen', function (modalType) {
+			assert.equal(modalType, 'search', 'onModalOpen should be called once search is toggled');
+		});
+
+		await render(hbs`
+		{{global-navigation/search
+			model=model.search
+			onModalOpen=onModalOpen
+			onSearchToggleClicked=onSearchToggleClicked
+		}}`);
+
+		await click('.wds-global-navigation__search-toggle-icon');
+	});
 });
