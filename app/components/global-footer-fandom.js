d2caff19e193760f9cc775c132fed021f8b2ca8b
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2019-04-05 15:35:11 +0200

    IW-1808 | Design Review applied

--- app/components/global-footer-fandom.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-import { oneWay } from '@ember/object/computed';
-import track from '../utils/wds-track';
-
-export default Component.extend({
-	tagName: 'footer',
-	classNames: 'wds-global-footer',
-});
